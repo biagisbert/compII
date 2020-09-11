01f9049b0261a9c8b6fa1fdee7449e99a469ad2e
bia_g <bia.gisbert0811@gmail.com>
2020-09-11 15:54:31 -0300

    Enviando questao 2 e codigos do ppt

--- TAC_1/testes/Questão_1.py
+++ TAC_1/testes/Questão_1.py
@@ -1,6 +1,7 @@
 refArquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\TcCLB.506717.80_AA.fasta")
-arquivoFasta = refArquivo.read()
-cabecalho = arquivoFasta.split('\n')[0][1:24]
-sequencia = ''.join(arquivoFasta.split('\n')[1:])
-print (("Identificador: %s"%cabecalho)),'\t',("Sequencia: %s"%sequencia)
+cabecalho = refArquivo.readline()[1:-1]
+sequencia = ""
+for linha in refArquivo:
+    sequencia += linha.replace('\n','')
+print (("Cabecalho: %s"%cabecalho)),'\t',("Sequencia: %s"%sequencia)
 refArquivo.close()
