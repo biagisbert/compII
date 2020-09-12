e84943870e0aa25049fe99c95ef1e3a3fc5107d3
bia_g <bia.gisbert0811@gmail.com>
2020-09-11 21:00:23 -0300

    Enviando de novo

--- TAC_1/testes/Questão_1.py
+++
@@ -1,7 +0,0 @@
-refArquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\TcCLB.506717.80_AA.fasta")
-cabecalho = refArquivo.readline()[1:-1]
-sequencia = ""
-for linha in refArquivo:
-    sequencia += linha.replace('\n','')
-print (("Cabecalho: %s"%cabecalho),'\t',("Sequencia: %s"%sequencia))
-refArquivo.close()
