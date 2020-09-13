5762fa3169979d864ec53e86b71f259515d7671d
bia_g <bia.gisbert0811@gmail.com>
2020-09-13 10:30:48 -0300

    fase final questao 2 e iniciando questao 3, teste sendo da questao 3

--- TAC_1/testes/Testes_1.py
+++
@@ -1,9 +0,0 @@
-refArquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta")
-cabecalho = refArquivo.readline()[0:-1]
-sequencia = ""
-for linha in refArquivo:
-    if '>' in linha:
-        print ("Cabecalho: %s"%linha)
-    else:
-        print ("Sequencia: %s"%linha)
-refArquivo.close()
