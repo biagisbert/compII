641794c47ceebc660a372d26a161ffe56cde6aa8
bia_g <bia.gisbert0811@gmail.com>
2020-09-13 11:07:12 -0300

    ok

--- TAC_1/sample/Questão_2.py
+++ TAC_1/sample/Questão_2.py
@@ -1,9 +1,12 @@
 refArquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta")
-cabecalho = refArquivo.readline()[0:-1]
+cabecalho = refArquivo.readline()[:]
 sequencia = ""
 for linha in refArquivo:
     if '>' in linha:
+        print("Sequencia: %s" % sequencia)
         print ("Cabecalho: %s"%linha)
+        sequencia = ""
     else:
-        print ("Sequencia: %s"%linha)
+        sequencia += linha.replace('\n','')
+print("Sequencia: %s" % sequencia)
 refArquivo.close()
