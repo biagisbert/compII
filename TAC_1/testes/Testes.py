3db8f9f93ae6191a2961e2703b672f715c94ddc8
bia_g <bia.gisbert0811@gmail.com>
2020-09-13 13:01:55 -0300

    ok

--- TAC_1/testes/Testes.py
+++ TAC_1/testes/Testes.py
@@ -1,8 +1,8 @@
 import csv
 with open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\species.csv") as ArquivoEntrada:
     ArquivoLeitura = csv.reader(ArquivoEntrada)
-    for linha in ArquivoLeitura:
+    for linha in ArquivoEntrada:
         data = linha.split(",")
-        if data[3].upper().rstrip() == "BIRD":
+        if data[3].upper() == "BIRD":
             print("%s\t%s\t%s\t%s" % (data[0], data[1], data[2], data[3].rstrip()))
     ArquivoEntrada.close()
