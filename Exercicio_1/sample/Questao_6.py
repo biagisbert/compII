9b39d0db4a3ef16440acb6bda076052f4ae49d62
bia_g <bia.gisbert0811@gmail.com>
2020-09-22 12:38:50 -0300

    ok

--- Exercicio_1/sample/Questao_6.py
+++ Exercicio_1/sample/Questao_6.py
@@ -2,4 +2,9 @@ import pandas as pd
 import xlrd
 ler = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\Exercicio_1\\dados\\WHO POP TB some.xls.xls")
 normalizar = ler['TB deaths'].mul(100)
-print (normalizar)
+taxa = normalizar/ler['Population (1000s)']
+pop = ler['Population (1000s)']/100
+nova = pd.DataFrame({'Country' : ler['Country'],
+                     'Population(100.000)' : pop,
+                    'Taxa de morte' : taxa})
+print (nova)
--- Exercicio_1/testes/Teste_1.py
+++ Exercicio_1/testes/Teste_1.py
@@ -1,4 +1,14 @@
+#Como os países possuem números populacionais variáveis,#
+#devemos normalizar a quantidade de óbitos pelo total de habitantes.#
+# Portanto, vamos normalizar a taxa de morte por 100.000 habitantes:#
+
 import pandas as pd
 import xlrd
 ler = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\Exercicio_1\\dados\\WHO POP TB some.xls.xls")
-print (ler)
+normalizar = ler['TB deaths'].mul(100)
+taxa = normalizar/ler['Population (1000s)']
+pop = ler['Population (1000s)']/100
+nova = pd.DataFrame({'Country' : ler['Country'],
+                     'Population(100.000)' : pop,
+                    'Taxa de morte' : taxa})
+print (nova)
