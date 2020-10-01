faa068865f6dfbf4e38f7ce354c05aa6a456e9a4
bia_g <bia.gisbert0811@gmail.com>
2020-10-01 18:53:55 -0300

    ok

--- TAC_2/sample/Codigos.py
+++ TAC_2/sample/Codigos.py
@@ -1,4 +1,11 @@
 import pandas as pd
-import xlrd
-ler = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
-    #Questão_1#
+import sys
+df = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
+tabela = sys.argv[1]
+coef_b = float(sys.argv[2])
+coef_m = float(sys.argv[3])
+df_q = df[['Target_Name', 'Stage']]
+df_q['Quantity'] = 10 ** ((df['CT'] - coef_b)/coef_m)
+df_final = pd.merge(df,df_q)
+df_final.to_excel(("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Tabela_Quant_Abs.xlsx"))
+print (df_final)
--- TAC_2/testes/Teste_Geral.py
+++ TAC_2/testes/Teste_Geral.py
@@ -1,12 +1,11 @@
-import pandas as pan
-import xlrd
+import pandas as pd
 import sys
+df = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
 tabela = sys.argv[1]
 coef_b = float(sys.argv[2])
 coef_m = float(sys.argv[3])
-df = pan.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
-df_q = df['Target_Name' , 'Stage']
+df_q = df[['Target_Name' , 'Stage']]
 df_q['Quantity'] = 10 ** ((df['CT'] - coef_b)/coef_m)
-df_final = pan.merge(df,df_q)
+df_final = pd.merge(df,df_q)
 df_final.to_excel(("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Tabela_Quant_Abs.xlsx"))
 print (df_final)
