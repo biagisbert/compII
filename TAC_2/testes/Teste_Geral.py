b803e2bb1a833260ef35eff8c5889bb4f7fe9c0a
bia_g <bia.gisbert0811@gmail.com>
2020-10-01 16:14:45 -0300

    Teste

--- TAC_2/testes/Teste_Geral.py
+++ TAC_2/testes/Teste_Geral.py
@@ -1,11 +1,12 @@
-import pandas as pd
+import pandas as pan
 import xlrd
 import sys
-arquivo = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
 tabela = sys.argv[1]
-coef_b = sys.argv[2]
-coef_m = sys.argv[3]
-print (sys.argv)
-
-
-
+coef_b = float(sys.argv[2])
+coef_m = float(sys.argv[3])
+df = pan.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
+df_q = df['Target_Name' , 'Stage']
+df_q['Quantity'] = 10 ** ((df['CT'] - coef_b)/coef_m)
+df_final = pan.merge(df,df_q)
+df_final.to_excel(("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Tabela_Quant_Abs.xlsx"))
+print (df_final)
