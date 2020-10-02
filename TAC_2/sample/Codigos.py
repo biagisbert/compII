4e8e11410f6ad33e99c22a0df7b351a91ae21303
bia_g <bia.gisbert0811@gmail.com>
2020-10-02 13:00:27 -0300

    Teste

--- TAC_2/sample/Codigos.py
+++ TAC_2/sample/Codigos.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import xlrd
 import sys
+Teste_Geral=sys.argv[0]
+tabela=sys.argv[1]
+coef_m=float(sys.argv[2])
+coef_b=float(sys.argv[3])
+m = -3.397186047
+b = 58.53223295
 df = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Valores_CT.xlsx")
-tabela = sys.argv[1]
-coef_b = float(sys.argv[2])
-coef_m = float(sys.argv[3])
-df_q = df[['Target_Name', 'Stage']]
-df_q['Quantity'] = 10 ** ((df['CT'] - coef_b)/coef_m)
+quantity = 10**((df['CT']-b)/m)
+df_q = pd.DataFrame({'Target_Name' : df['Target_Name'] ,
+                     'Stage' : df['Stage'] ,
+                     'Quantity': quantity})
 df_final = pd.merge(df,df_q)
-df_final.to_excel(("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados\\Tabela_Quant_Abs.xlsx"))
+df_final.to_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_2\\dados" , sheet_name="Ct_Abs")
 print (df_final)
