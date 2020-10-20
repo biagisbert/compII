baeaa2b1736f34d85103e4d0c76bfb5909489f69
bia_g <bia.gisbert0811@gmail.com>
2020-10-19 23:15:14 -0300

    ok

--- TAC_3/sample/Questao_2.py
+++ TAC_3/sample/Questao_2.py
@@ -1,10 +1,7 @@
-#Escreva um programa Python que pegue o arquivo sequencias.fasta e
-#escreva N arquivos FASTA contendo em cada arquivo apenas uma sequência
-#de sequencias.fasta, chamados de sequência_i.fasta (onde i varia de 1 a N).
-## exemplo: arquivo FASTA que contém duas sequências
-#>seq1
-#AAAAA
-#>seq2
-#TTTTT
-### vocês vão criar 2 arquivos chamados sequencia_1.fasta e sequencia_2.fasta
-### onde cada um dos arquivos tem apenas uma sequencia
+from Bio import SeqIO
+arquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\sequencias.fasta")
+for i in SeqIO.parse(arquivo,"fasta"):
+    arg = ("sequencia_"+i.id).__str__()
+    new = SeqIO.write(i,arg,"fasta")
+print('fim')
+##Salvaram 837 arquivos na pasta do sample, transferi eles manualmente para os dados##
