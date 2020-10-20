1ffc2628661615a22b54069195313bcd23990415
bia_g <bia.gisbert0811@gmail.com>
2020-10-20 16:03:38 -0300

    ok

--- TAC_3/sample/Questao_2.py
+++ TAC_3/sample/Questao_2.py
@@ -1,7 +1,9 @@
 from Bio import SeqIO
 arquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\sequencias.fasta")
+count = 1
 for i in SeqIO.parse(arquivo,"fasta"):
-    arg = ("sequencia_"+i.id).__str__()
+    arg = ("sequencia_")+count.__str__()
     new = SeqIO.write(i,arg,"fasta")
+    count = count+1
 print('fim')
 ##Salvaram 837 arquivos na pasta do sample, transferi eles manualmente para os dados##
