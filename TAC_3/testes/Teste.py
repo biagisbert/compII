cc34d36383c76d58e288364b49c9e26319076547
bia_g <bia.gisbert0811@gmail.com>
2020-10-19 23:15:53 -0300

    testando

--- TAC_3/testes/Teste.py
+++ TAC_3/testes/Teste.py
@@ -1,15 +1,7 @@
-#Usar o arquivo sequencias.fasta
-#O programa tem que criar arquivos e em cada arquivo
-# precisa que tenha uma sequencia, chamando de
-# sequencia_i.fasta(onde i varia de 1 a N)#
-## exemplo: arquivo FASTA que contém duas sequências
-#>seq1=AAAAA
-#>seq2=TTTTT
-## vocês vão criar 2 arquivos chamados sequencia_1.fasta e sequencia_2.fasta
-## onde cada um dos arquivos tem apenas uma sequencia
-from Bio.Seq import Seq
-from Bio import SeqIO
-arquivo = open("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\sequencias.fasta")
-for i in SeqIO.parse(arquivo,"fasta"):
-    new = SeqIO.write(i,"sequencia_i.fasta","fasta")
-print('fim')
+#Considerando o arquivo FASTA sequenciaDesconhecida.fasta,
+# faça um código em Python que:
+##Lê do usuário o caminho para o arquivo FASTA sequenciaDesconhecida.fasta;
+##Lê do usuário o caminho para o arquivo multi-fasta de proteínas
+#de Trypanosoma cruzi cepa CL Brener Esmeraldo-like TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta;#
+##Executa BLAST da sequência desconhecida de DNA contra as sequências de aminoácidos usando formato de saída nº 6 e e-valor igual à 0,05
+##Imprima somente o hit com o maior score.#
