1056b2e23f01fd13c4bd2551d1643c601eb948e9
bia_g <bia.gisbert0811@gmail.com>
2020-10-21 22:29:12 -0300

    ok

--- TAC_3/sample/Questao_3.py
+++ TAC_3/sample/Questao_3.py
@@ -1,7 +1,10 @@
-#Considerando o arquivo FASTA sequenciaDesconhecida.fasta,
-# faça um código em Python que:
-##Lê do usuário o caminho para o arquivo FASTA sequenciaDesconhecida.fasta;
-##Lê do usuário o caminho para o arquivo multi-fasta de proteínas
-#de Trypanosoma cruzi cepa CL Brener Esmeraldo-like TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta;#
-##Executa BLAST da sequência desconhecida de DNA contra as sequências de aminoácidos usando formato de saída nº 6 e e-valor igual à 0,05
-##Imprima somente o hit com o maior score.#
+import pandas as pd
+from Bio.Blast.Applications import NcbiblastxCommandline
+blast_path = "C:\\Program Files\\NCBI\\blast-2.10.1+\\bin\\blastx.exe"
+arquivo_out = "C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\out.blastp.outfmt6.fasta"
+unknow_seq = open(input("Digite o caminho para a Sequencia desconhecida: "))
+alvo_seq = open(input("Digite o caminho para a Sequencia alvo: "))
+blast_line = NcbiblastxCommandline(cmd=blast_path, query=unknow_seq, subject=alvo_seq, outfmt=6, out= arquivo_out, evalue=0.05)
+result_blast = pd.read_csv(arquivo_out, sep='\t', names=["qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend","sstart","send","evalue","bitscore"])
+max_hit = result_blast.sort_values('bitscore')
+print(max_hit.iloc[[-1]])
--- TAC_3/testes/Teste.py
+++ TAC_3/testes/Teste.py
@@ -1,7 +1,18 @@
-#Considerando o arquivo FASTA sequenciaDesconhecida.fasta,
-# faça um código em Python que:
-##Lê do usuário o caminho para o arquivo FASTA sequenciaDesconhecida.fasta;
-##Lê do usuário o caminho para o arquivo multi-fasta de proteínas
-#de Trypanosoma cruzi cepa CL Brener Esmeraldo-like TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta;#
 ##Executa BLAST da sequência desconhecida de DNA contra as sequências de aminoácidos usando formato de saída nº 6 e e-valor igual à 0,05
 ##Imprima somente o hit com o maior score.#
+# C://Users//bia_g//PycharmProjects//compII//TAC_3//dados//sequenciaDesconhecida.fasta
+# C://Users//bia_g//PycharmProjects//compII//TAC_3//dados//TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta
+import pandas as pd
+from Bio.Blast import Applications
+from Bio.Blast.Applications import NcbiblastxCommandline
+
+blast_path = "C:\\Program Files\\NCBI\\blast-2.10.1+\\bin\\blastp"
+output =str("C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\out.blastp.outfmt6.fasta")
+unknow_seq = open("C://Users//bia_g//PycharmProjects//compII//TAC_3//dados//sequenciaDesconhecida.fasta")
+seq_alvo = open("C://Users//bia_g//PycharmProjects//compII//TAC_3//dados//TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta")
+print(unknow_seq)
+blast_line = NcbiblastxCommandline(cmd=blast_path, query=unknow_seq, subject=seq_alvo, outfmt=6, out=output, evalue=0.05)
+stdout = output
+result_blast = pd.read_csv(output,sep='\t', names=["qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend","sstart","send","evalue","bitscore"])
+max_hit = result_blast.sort_values('bitscore')
+print(seq_alvo)
