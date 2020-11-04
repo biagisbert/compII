4630252491e9620befdd79c1e46b8dd9edd55954
biagisbert <70611295+biagisbert@users.noreply.github.com>
2020-11-04 12:55:50 -0300

    Update Teste.py
    ok

--- TAC_4/testes/Teste.py
+++ TAC_4/testes/Teste.py
@@ -1,9 +1,18 @@
 import re
-from Bio.Seq import Seq
 from Bio import SeqIO
-#input do motivo
-motivo = str(input("Digite sua sequencia: "))
-arquivo = "C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_3\\dados\\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta"
-
-for record in SeqIO.parse(arquivo, "fasta"):
-    print("ID %s" % record.id)
+motivo = input("Digite sua sequencia: ").upper()
+arquivo = "C:\\Users\\bia_g\\PycharmProjects\\compII\\TAC_1\\dados\\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta"
+ref_arquivo = SeqIO.parse(arquivo, "fasta")
+count = 0
+if re.search('^[ACGT]+$',motivo):
+    print("É uma sequencia de DNA")
+    for line in ref_arquivo:
+        print("ID %s" % str(line.id))
+        count = count +1
+    print("O motivo se repete %s vezes" % count)
+else:
+    print("É uma sequencia de aminoácidos")
+    for line in ref_arquivo:
+        print("ID %s" % str(line.id))
+        count = count + 1
+    print("O motivo se repete %s vezes" % count)
