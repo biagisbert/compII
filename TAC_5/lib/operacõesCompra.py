0a4baa7105f736f3e05bf425f3c0d1c454954090
bia_g <bia.gisbert0811@gmail.com>
2020-11-14 21:13:19 -0300

    ok

--- TAC_5/lib/operacõesCompra.py
+++ TAC_5/lib/operacõesCompra.py
@@ -1,12 +1,18 @@
 def imprimeProdutos(dic):
+    print("Relação de Produtos")
     for e in dic:
         print(e)
 
 def imprimeQuantidades(dic):
+    print("Relação de Produtos e Quantidade")
     for e in dic:
         print(e, '\t', dic[e][1])
 
 def calculaTotalCompra(dic):
+    print("Relação de Produtos e Preço")
+    total = 0
     for e in dic:
-        total = dic[e][0] * dic[e][1]
+        valor = dic[e][0] * dic[e][1]
+        total = total + valor
         print(e, '\t', total)
+    print("Total da compra: %s" %total)
--- TAC_5/sample/controleAquisicaoBioMol.py
+++ TAC_5/sample/controleAquisicaoBioMol.py
@@ -1,33 +1,18 @@
-##Escreva um programa chamado
-# “controleAquisicaoBioMol.py” que leia uma
-# relação de materiais de laboratório de
-# biologia molecular digitada pelo usuário
-# que serão consultados para aquisição.
+import TAC_5.lib.operacõesCompra as tc
 
-#1-O programa vai ler produto, preço e
-# quantidade até que o usuário digite “-1”
-# para parar.
+dic = {}
+while True:
+    produto = input("Digite o produto: ")
+    if produto == "-1":
+        break
+    preco = float(input("Digite o preço: "))
+    if preco == -1:
+        break
+    quantidade = int(input("Digite a quantidade: "))
+    if quantidade == -1:
+        break
+    dic[produto] = [preco, quantidade]
 
-#2-O programa armazenará em um dicionário
-# onde o produto será a chave referente à
-# uma lista contendo o preço e quantidade
-# deste produto, ou seja, a chave será
-# “produto” e o valor será uma lista
-# [preço, quantidade]. Exemplo:
-#estoque = {“caixa de ponteira” : [20, 3], “caixa de par de luvas”: [35.5, 4], “máscara” : [2, 200], “pipeta” : [1500, 3]}
-
-#3-Escreva um módulo em Python chamado
-# “operacoesCompra.py” que conterá as
-# funções:
-#imprimeProdutos: imprime o nome dos
-# produtos a serem comprados;
-#imprimeQuantidades: imprime a quantidade
-# de cada produto a ser comprado;
-#calculaTotalCompra: retorna o valor total
-# das compras;
-
-#4-O seu programa principal
-# “controleAquisicaoBioMol.py” deverá
-# importar “operacoesCompra.py” para mostrar
-# ao usuário o nome dos produtos, a
-# quantidade e o valor total de compras.
+prod = tc.imprimeProdutos(dic)
+quant = tc.imprimeQuantidades(dic)
+calculo = tc.calculaTotalCompra(dic)
