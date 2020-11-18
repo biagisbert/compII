11e3ec9cc30f94c875b1792e7fac83202755b631
bia_g <bia.gisbert0811@gmail.com>
2020-11-18 15:57:03 -0300

    testando

--- Projeto_Final/teste/Teste.py
+++ Projeto_Final/teste/Teste.py
@@ -10,14 +10,13 @@ Tabela_1 = sys.argv[1]
 seq_desconhecida = sys.argv[2]
 arquivo_multi = sys.argv[3]
 
-#(Tabela_1 = sys.argv[1]; seq_desconhecida = sys.argv[2]; arquivo_multi = sys.argv[3])
-#Tabela_1
+# (Tabela_1 = sys.argv[1]; seq_desconhecida = sys.argv[2]; arquivo_multi = sys.argv[3])
+# Tabela_1
 dt = pd.read_excel("C:\\Users\\bia_g\\PycharmProjects\\compII\\Projeto_Final\\dados\\Tabela_1.xlsx")
-#seq_desconhecida
+# seq_desconhecida
 dd = SeqIO.parse("C:\\Users\\bia_g\\PycharmProjects\\compII\\Projeto_Final\\dados\\Rdesconhecidus.fasta", "fasta")
-#arquivo_multi
-dm = SeqIO.parse("C:\\Users\\bia_g\\PycharmProjects\\compII\\Projeto_Final\\dados\\VectorBase-48_RprolixusCDC_Annotated"
-                 "Proteins.fasta", "fasta")
+# arquivo_multi
+dm = SeqIO.parse("C:\\Users\\bia_g\\PycharmProjects\\compII\\Projeto_Final\\dados\\VectorBase-48_RprolixusCDC_AnnotatedProteins.fasta", "fasta")
 
 fatorNormalizado_Rep1_A = 10**6/dt['Rep1_A'].sum()
 fatorNormalizado_Rep2_A = 10**6/dt['Rep2_A'].sum()
@@ -31,8 +30,7 @@ data_normalizado = {
   'Rep1_B_CPM': dt['Rep1_B'] * fatorNormalizado_Rep1_B,
   'Rep2_B_CPM': dt['Rep2_B'] * fatorNormalizado_Rep2_B}
 
-dt_normalizado = pd.DataFrame(data_normalizado, columns=['gene_id', 'Rep1_A_CPM', 'Rep2_A_CPM', 'Rep1_B_CPM',
-                                                        'Rep2_B_CPM'])
+dt_normalizado = pd.DataFrame(data_normalizado, columns=['gene_id', 'Rep1_A_CPM', 'Rep2_A_CPM', 'Rep1_B_CPM', 'Rep2_B_CPM'])
 
 df = pd.merge(dt, dt_normalizado)
 
@@ -45,8 +43,7 @@ data_media = {
   'Cond_A_CPM_media': ((df['Rep1_A_CPM']+df['Rep2_A_CPM'])/2),
   'Cond_B_CPM_media': ((df['Rep1_B_CPM']+df['Rep2_B_CPM'])/2)}
 
-df_media = pd.DataFrame(data_media, columns=['gene_id', 'Rep1_A_CPM', 'Rep2_A_CPM', 'Rep1_B_CPM', 'Rep2_B_CPM',
-                                            'Cond_A_CPM_media', 'Cond_B_CPM_media'])
+df_media = pd.DataFrame(data_media, columns=['gene_id', 'Rep1_A_CPM', 'Rep2_A_CPM', 'Rep1_B_CPM', 'Rep2_B_CPM', 'Cond_A_CPM_media', 'Cond_B_CPM_media'])
 
 df_final = pd.merge(df, df_media)
 
@@ -55,50 +52,13 @@ asc_B = pd.DataFrame(df_final.nlargest(5, 'Cond_B_CPM_media'))
 id_gene = asc_A['gene_id'].append(asc_B['gene_id'])
 gene_cond = list(id_gene)
 
-for i in gene_cond:
-     nomeArq = str("{}.fasta").format(i)
-     refArquivoSaida = open(nomeArq,"w")
-     SeqIO.write(i, refArquivoSaida, "fasta")
-     refArquivoSaida.close()
-     sequencia = nomeArq
-     refArquivoSaida.close()
+gene_blast = []
 
-     linha_de_comando_blastx = NcbiblastxCommandline(cmd=blastx_path, query=sequencia, subject=dm,
-                                                         outfmt=6, out=meuOutput, evalue=0.05)
-     stdout, stderr = linha_de_comando_blastx()
-
-
-blast_result = open(meuOutput, "r")
-
-qseqid = 0
-sseqid = 1
-pident = 2
-length = 3
-mismatch = 4
-gapopen = 5
-qstart = 6
-qend = 7
-sstart = 8
-send = 9
-evalue = 10
-bitscore = 11
-
-results = blast_result.read()
-s = results.split('\n')
-besthit = '0'
-Evalue = 0
-encontrada = '0'
-
-for linha in s:
-  hit = linha.split('\t')
-  if len(hit) != 12:
-      break
-  if float(besthit) < float(hit[bitscore]):
-      busca = hit[qseqid]
-      besthit = hit[bitscore]
-      Evalue = hit[evalue]
-      encontrada = hit[sseqid]
-
-  print("Sequência encontrada: %s" % encontrada)
-  print("Score = %s" % besthit)
-  print("E-value = %s" % Evalue)
+for i in dd:
+    for e in gene_cond:
+        if i.id == e:
+            gene_blast.append(i.id)
+            gene_blast.append(i.seq)
+        else:
+            continue
+print("gene %s"%gene_blast)
